56da738e0983fdff0a60279ab298d4013ebba60f
fabiobrasileiroo <fabio.h591@gmail.com>
2024-04-18 16:58:00 -0400

    buffer

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -6,7 +6,7 @@ class OneToHundreStream extends Readable {
     const i = this.index++
     setTimeout(() => {
 
-      if (i > 100) {
+      if (i > 5) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i))
@@ -19,5 +19,33 @@ class OneToHundreStream extends Readable {
 fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundreStream(),
-
+  duplex: 'half'
+}).then(response => {
+  return response.text()
+}).then(data => {
+  console.log(data)
 })
+// import { Readable } from 'node:stream'
+// import fetch from 'node-fetch'
+
+// class OneToHundreStream extends Readable {
+//   index = 1
+//   _read() {
+//     const i = this.index++
+//     setTimeout(() => {
+//       if (i > 100) {
+//         this.push(null)
+//       } else {
+//         const buf = Buffer.from(String(i))
+//         this.push(buf)
+//       }
+//     }, 1000)
+//   }
+// }
+
+// fetch('http://localhost:3334', {
+//   method: 'POST',
+//   body: new OneToHundreStream(),
+//   // Passando a opção duplex
+//   duplex: true
+// })
