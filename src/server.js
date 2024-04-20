e8fb9b35edb3d364d9a93ee35d9c740c431f0fb0
fabiobrasileiroo <fabio.h591@gmail.com>
2024-04-20 14:31:13 -0400

    estudo

--- src/server.js
+++ src/server.js
@@ -1,38 +1,23 @@
 import http from 'node:http'
+import { json } from './middleware/json.js'
+import { routes } from './routes.js'
 // StateFul != Stateless
 //requisicao = req e resposta = res
+
+
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
-    const buffers = []
+    await json(req,res)
 
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    }
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.bod = null
-    }
-
-
-    if (method === 'GET' && url === '/users') {
-        // Early return ou seja se bate no return ou chega no return ele ignora tudo em baixo
-        return res
-            .setHeader('Content-type', 'application/json')
-            .end(JSON.stringify(users))
-    }
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body
-        users.push({
-            id: 1,
-            name,
-            email
-        })
-        return res.writeHead(201).end('Criação de usuário')
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
+    if (route) {
+        return route.handler(req, res)  
     }
     return res.writeHead(404).end('Not found')
 })
